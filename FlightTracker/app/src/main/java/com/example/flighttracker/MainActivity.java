ae4f9b25801371bd12529e7256c3d3c2bade5679
Farhana <farhana.mahmud.algonquin@gmail.com>
2023-07-18 09:09:28 +0530

    AlertDialog implementation code

--- FlightTracker/app/src/main/java/com/example/flighttracker/MainActivity.java
+++ FlightTracker/app/src/main/java/com/example/flighttracker/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.flighttracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,5 +56,35 @@ public class MainActivity extends AppCompatActivity {
         data.add(new Flight("A","B","Schedule"));
         data.add(new Flight("A","B","Schedule"));
         return data;
+    }
+
+
+    void createToast(Context context, String message)
+    {
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+    void snackBar(Context context, String message, View view)
+    {
+        Snackbar.make(context,view,message,Snackbar.LENGTH_LONG).show();
+    }
+
+    void showAlertDialog(Context context,String title,String message,String[] buttonTitles)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                                .setTitle(title).setMessage(message).setPositiveButton(buttonTitles[0], new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setNegativeButton(buttonTitles[1], new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
     }
 }
